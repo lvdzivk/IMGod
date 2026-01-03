a7fd734cc5a5edbda99347475fa2f34529457196
L4dzi <marcin@ladziak.tech>
2026-01-03 22:05:14 +0000

    modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let currentFile = null;
+let currentBuffer = null;
+let currentMeta = null;
+let currentCleanBlob = null;
 
 // Creates invisible <input type="file">
 const fileInput = document.createElement('input');
@@ -11,12 +15,11 @@ document.querySelector('.uploadPreviewPanel').addEventListener('click', function
     fileInput.click();
 });
 
-
 fileInput.addEventListener('change', function(event) {
     const file = event.target.files[0];
     if (!file) return;
 
-    console.log('Selected image: ', file.name);
+        currentFile = file;
 
     // file reading
     const reader = new FileReader();
@@ -24,6 +27,8 @@ fileInput.addEventListener('change', function(event) {
     reader.onload = function(e) {
         const buffer = e.target.result;
         console.log('Bytes:', buffer.byteLength);
+
+        currentBuffer = buffer;
 
         // exifr functions - check tiff, iptc and xmp and returns inside HTML
         exifr.parse(buffer, {
@@ -33,6 +38,8 @@ fileInput.addEventListener('change', function(event) {
         }).then(function(meta) {
             document.querySelector('.metadataList').innerHTML = '';
 
+            currentMeta - meta;
+
             //loop for checking all information and returing them
             for (let key in meta) {
                 if (meta[key]) {
@@ -41,8 +48,40 @@ fileInput.addEventListener('change', function(event) {
                     document.querySelector('.metadataList').appendChild(div);
                 }
             }
+
+             
         });
     };
 
     reader.readAsArrayBuffer(file);
 });
+
+document.getElementById('wipeButtom').addEventListener('click', function() {
+    if (!currentFile) {
+        alert('Upload image first!')
+        return;
+    }
+
+    // Canvas - creates clean image
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    const img = new Image();
+
+    img.onload = function() {
+        // Set resolution
+        canvas.width = img.width;
+        canvas.height = img.height;
+
+        // Draw image without meta
+        ctx.drawImage(img, 0, 0);
+
+        // Save as new file
+        canvas.toBlob(function(blob) {
+            console.log('Clean image:', blob.size, 'bytes');
+            currentCleanBlob - blob;
+        }, 'image/jpeg', 0.95); // Quality 95%
+    };
+
+    img.src = URL.createObjectURL(currentFile);
+});
+
